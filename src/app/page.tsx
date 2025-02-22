d4eef4e12b382e7a5dbcec9d7e6941211065f861
nicolagheza <ghzncl@gmail.com>
2025-02-22 03:15:10 +0100

    Anal~plausible

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
 import Links from '@/components/links'
+import PlausibleProvider from 'next-plausible'
 
 export default function Home() {
   return (
-    <div className="font-base">
-      <h1 className="text-2xl font-heading sm:text-4xl">Nicode</h1>
-      <p className="mt-2 text-lg sm:text-xl">Software Engineer</p>
+    <PlausibleProvider domain="nicolagheza.com">
+      <div className="font-base">
+        <h1 className="text-2xl font-heading sm:text-4xl">Nicode</h1>
+        <p className="mt-2 text-lg sm:text-xl">Software Engineer</p>
 
-      <div className="mt-8 text-base sm:text-lg">
-        <p>Hi, my name is Nicola Gheza, but you can call me <u>Nicode!</u></p>
-        <p>I am a software engineer based in Valle Camonica, Italy 🏔️🇮🇹.</p>
+        <div className="mt-8 text-base sm:text-lg">
+          <p>
+            Hi, my name is Nicola Gheza, but you can call me <u>Nicode!</u>
+          </p>
+          <p>I am a software engineer based in Valle Camonica, Italy 🏔️🇮🇹.</p>
+        </div>
+        <Links />
       </div>
-      <Links />
-    </div>
+    </PlausibleProvider>
   )
 }
