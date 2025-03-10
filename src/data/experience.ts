9ad7d3f8d489e6ff56f4d97762402b44ad596109
nicolagheza <ghzncl@gmail.com>
2025-03-10 22:04:02 +0100

    Knowledge Engineering

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -5,48 +5,48 @@ const PAST_ROLES: {
   endDate: string
   location: string
 }[] = [
-    {
-      company: 'Sport it',
-      role: 'Backend Developer',
-      startDate: 'March 2022',
-      endDate: 'Now',
-      location: 'Remote',
-    },
-    {
-      company: 'SteelTrace B.V.',
-      role: 'Software Engineer',
-      startDate: 'August 2020',
-      endDate: 'October 2021',
-      location: 'Maastricht (NL)',
-    },
-    {
-      company: 'CX Company [now cm.com]',
-      role: 'Backend Developer',
-      startDate: 'February 2019',
-      endDate: 'August 2020',
-      location: 'Maastricht (NL)',
-    },
-    {
-      company: 'Consense Data Exchange',
-      role: 'Software Engineer',
-      startDate: 'July 2018',
-      endDate: 'December 2018',
-      location: 'Heerlen (NL)',
-    },
-    {
-      company: 'WildFire Learning',
-      role: 'Software Engineer',
-      startDate: 'July 2017',
-      endDate: 'December 2017',
-      location: 'Brighton (UK)',
-    },
-    {
-      company: 'nicode.solutions',
-      role: 'Software Engineer',
-      startDate: 'January 2015',
-      endDate: 'Now',
-      location: 'Remote',
-    },
-  ]
+  {
+    company: 'nicode.solutions',
+    role: 'Knowledge Engineer',
+    startDate: 'January 2015',
+    endDate: 'Now',
+    location: '🌎',
+  },
+  {
+    company: 'Sport it',
+    role: 'Backend Developer',
+    startDate: 'March 2022',
+    endDate: 'March 2025',
+    location: 'Remote (IT)',
+  },
+  {
+    company: 'SteelTrace B.V.',
+    role: 'Software Engineer',
+    startDate: 'August 2020',
+    endDate: 'October 2021',
+    location: 'Maastricht (NL)',
+  },
+  {
+    company: 'CX Company [now cm.com]',
+    role: 'Backend Developer',
+    startDate: 'February 2019',
+    endDate: 'August 2020',
+    location: 'Maastricht (NL)',
+  },
+  {
+    company: 'Consense Data Exchange',
+    role: 'Software Engineer',
+    startDate: 'July 2018',
+    endDate: 'December 2018',
+    location: 'Heerlen (NL)',
+  },
+  {
+    company: 'WildFire Learning',
+    role: 'Software Engineer',
+    startDate: 'July 2017',
+    endDate: 'December 2017',
+    location: 'Brighton (UK)',
+  },
+]
 
 export default PAST_ROLES
