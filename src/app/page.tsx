cbc7de510ce3ab06c5b9bdaff2d1a7967d2130ac
nicolagheza <ghzncl@gmail.com>
2025-04-28 22:31:18 +0200

    Changes homepage text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,20 @@ export default function Home() {
   return (
     <PlausibleProvider domain="nicolagheza.com">
       <div className="font-base">
-        <h1 className="text-2xl font-heading sm:text-4xl">Nicode</h1>
+        <h1 className="text-2xl font-heading sm:text-4xl">Nicola Gheza</h1>
         <p className="mt-2 text-lg sm:text-xl">
-          Data Science &amp; Artificial Intelligence
+          Software Engineer | Data Science &amp; Artificial Intelligence
         </p>
 
         <div className="mt-8 text-base sm:text-lg">
           <p>
             Hi, my name is Nicola Gheza, but you can call me <u>Nicode!</u>
           </p>
-          <p>I am a software engineer based in Valle Camonica, Italy 🏔️🇮🇹.</p>
+          <p>
+            I&apos;m a software engineer based in Valle Camonica, Italy 🏔️🇮🇹,
+            passionate about building solutions at the intersection of software
+            development, data science, and artificial intelligence.
+          </p>
         </div>
         <Links />
       </div>
